7b3d98ba0b315981c139a326dbab32b7a643acff
anthow <anthonyenglebert@gmail.com>
2021-12-02 12:25:53 +0100

    /

--- src/pages/devenir-benevole.js
+++ src/pages/devenir-benevole.js
@@ -14,8 +14,9 @@ const devenirBenevole = () => (
             <h1 className="font-black text-4xl  text-vert-interra  bg-white text-center"> Devenir bénévole </h1>
 
                         
-            <iframe width="540" height="2305" src="https://ba8902f1.sibforms.com/serve/MUIEAK4XuVKs65kbXEawmECYBvRXJmLFdkpoI5c3KUJ0pZeit1nVCIWXPwWQPpjavA5F7nWGj1Rj3P7kImA11-xOTgCNb01V_LXnrHXirGmdiSqWFphIPK6zouGQ7w3XRUYqhet0dh0fKm8AdIwknDLtG5YkR0FRnA3LOqvishQRafl8oK19DHYAPuZnyn7F06FBPpGNhLqifDI_" frameborder="0" scrolling="auto" allowfullscreen style="display: block;margin-left: auto;margin-right: auto;max-width: 100%;"></iframe>                             
-            </article>
+            <iframe width="540" height="2305" 
+            src="https://ba8902f1.sibforms.com/serve/MUIEAK4XuVKs65kbXEawmECYBvRXJmLFdkpoI5c3KUJ0pZeit1nVCIWXPwWQPpjavA5F7nWGj1Rj3P7kImA11-xOTgCNb01V_LXnrHXirGmdiSqWFphIPK6zouGQ7w3XRUYqhet0dh0fKm8AdIwknDLtG5YkR0FRnA3LOqvishQRafl8oK19DHYAPuZnyn7F06FBPpGNhLqifDI_" 
+            frameborder="0" scrolling="auto" allowfullscreen styles="display: block;margin-left: auto;margin-right: auto;max-width: 100%;"></iframe>              </article>
         </div>
     </Layout>
 )
